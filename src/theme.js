da18c60b3690a3d283b42d52cf5bf9f38d174387
John Mai <44597440+JohnMai1994@users.noreply.github.com>
2021-01-02 18:05:57 -0500

    添加样式 整体样式theme.js 还有部分style.css

---
+++ src/theme.js
@@ -0,0 +1,18 @@
+export default {
+    primaryColor : "#577BF9",
+    green: "#6FC416",
+    yellow: "#F7B500",
+    blue: "#54B5FF",
+    red: "#FF6364",
+    white: "#EBF5FB",
+
+    grayDark: "#181C2F",
+    gray2: "rgba(241, 237, 237, 0.3)",
+    gray3: "rgba(24, 28, 47, 0.3)",
+    gray4: "#EFECE8",
+    gray5: "#D8D8D8",
+
+    background: "white",
+
+
+}
