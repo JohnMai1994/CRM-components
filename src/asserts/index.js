bf814e2952b2f8fb31d3afc08d1f0820b2cf0096
John Mai <44597440+JohnMai1994@users.noreply.github.com>
2021-01-02 18:03:28 -0500

    添加图标，并封装其在assert之中

---
+++ src/asserts/index.js
@@ -0,0 +1,58 @@
+import {ReactComponent as Alert} from "../asserts/cuc_logo/icon_alert.svg";
+import {ReactComponent as AddCircle} from "../asserts/cuc_logo/addCircle.svg";
+import {ReactComponent as AddRect} from "../asserts/cuc_logo/addRect.svg";
+import {ReactComponent as Document} from "../asserts/cuc_logo/docu.svg";
+import {ReactComponent as Image} from "../asserts/cuc_logo/image.svg";
+import {ReactComponent as Import} from "../asserts/cuc_logo/import.svg";
+import {ReactComponent as More} from "../asserts/cuc_logo/more.svg";
+import {ReactComponent as Notification} from "../asserts/cuc_logo/notification.svg";
+import {ReactComponent as SearchLarge} from "../asserts/cuc_logo/searchLarge.svg";
+import {ReactComponent as SearchSmall} from "../asserts/cuc_logo/searchSmall.svg";
+import {ReactComponent as Unreco} from "../asserts/cuc_logo/unreco.svg";
+
+// Arrow
+import {ReactComponent as ArrowRight} from "../asserts/cuc_logo/Arrow/arrowRight.svg";
+import {ReactComponent as ArrowDoubleRight} from "../asserts/cuc_logo/Arrow/arrowDoubleRight.svg";
+import {ReactComponent as ArrowDown} from "../asserts/cuc_logo/Arrow/arrowDown.svg";
+import {ReactComponent as ArrowLeft} from "../asserts/cuc_logo/Arrow/arrowLeft.svg";
+
+// National
+import {ReactComponent as Canada} from "../asserts/cuc_logo/National/canada.svg";
+import {ReactComponent as China} from "../asserts/cuc_logo/National/china.svg";
+import {ReactComponent as USA} from "../asserts/cuc_logo/National/united-states.svg";
+
+// NavBarIcon
+import {ReactComponent as Dashboard} from "../asserts/cuc_logo/NavBarIcon/dashboard.svg";
+import {ReactComponent as Calander} from "../asserts/cuc_logo/NavBarIcon/calander.svg";
+import {ReactComponent as Contact} from "../asserts/cuc_logo/NavBarIcon/contact.svg";
+import {ReactComponent as Finance} from "../asserts/cuc_logo/NavBarIcon/finance.svg";
+import {ReactComponent as Management} from "../asserts/cuc_logo/NavBarIcon/management.svg";
+import {ReactComponent as Message} from "../asserts/cuc_logo/NavBarIcon/message.svg";
+import {ReactComponent as Personnel} from "../asserts/cuc_logo/NavBarIcon/personnel.svg";
+import {ReactComponent as Project} from "../asserts/cuc_logo/NavBarIcon/project.svg";
+import {ReactComponent as Setting} from "../asserts/cuc_logo/NavBarIcon/setting.svg";
+
+// Timeline
+import {ReactComponent as History} from "../asserts/cuc_logo/Timeline/history.svg";
+import {ReactComponent as Next} from "../asserts/cuc_logo/Timeline/next.svg";
+import {ReactComponent as Register} from "../asserts/cuc_logo/Timeline/register.svg";
+
+// Translation
+import {ReactComponent as CN} from "../asserts/cuc_logo/Translation/CN.svg";
+import {ReactComponent as EN} from "../asserts/cuc_logo/Translation/EN.svg";
+
+
+
+// Export All SVG ICON
+export {
+    Alert, Image, AddCircle, AddRect, Document, Import, More, Notification, SearchLarge, SearchSmall, Unreco,
+    // Arrow
+    ArrowDown, ArrowDoubleRight, ArrowLeft, ArrowRight,
+    // National
+    Canada, China, USA,
+    // Narbar
+    Calander, Message, Management, Project, Personnel, Contact, Finance, Dashboard, Setting,
+    // Timeline
+    History, Next, Register,
+    CN, EN
+}
